7c78ca703076acccc5923f6dec63402210382e73
choyeon5901-cell <choyeon5901@gmail.com>
2026-08-20 18:18:17 +0900

    Add files via upload
    게시판 메뉴삭제 및 css 깨짐 보완

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const heroVideo = document.querySelector('[data-hero-video]');
 const heroSlideshow = document.querySelector('[data-hero-slideshow]');
 const heroCaption = document.querySelector('[data-hero-caption]');
 const HERO_SLIDE_MS = 6500;
-const NAV_DRAWER_MQ = window.matchMedia('(max-width: 1680px)');
+const NAV_DRAWER_MQ = window.matchMedia('(max-width: 1560px)');
 let heroSlideTimer = null;
 let heroLoopActive = true;
 let navBodyOverflowPrev = '';
@@ -26,7 +26,7 @@ let navMenuHomeParent = navInner;
 let navMenuHomeNext = null;
 
 function isNavDrawerMode() {
-    return true;
+    return NAV_DRAWER_MQ.matches;
 }
 
 function placeNavMenuForViewport() {
